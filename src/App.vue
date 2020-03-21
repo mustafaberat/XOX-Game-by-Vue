0325422657b0e2cfc03a42b41a565d1a2a86fba8
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-03-22 00:08:04 +0300

    first commit - add table only

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GameComponent/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GameComponent from './components/GameComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GameComponent
   }
 }
 </script>
---
+++ src/components/GameComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+  <table>
+    <tr v-bind:key="row.id" v-for="row in board">
+      <td v-bind:key="cell.id" v-for="cell in row">{{ cell }}</td>
+    </tr>
+  </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameComponent',
+  data: function(){
+    return {
+        board: [
+          ['','',''],
+          ['','',''],
+          ['','',''],
+          // ['X','X','X'],
+          // ['X','X','X'],
+          // ['X','X','X'],
+        ]
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
